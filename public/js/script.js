d5a52537b570feb0f4a8ea05652e7b246fd0feed
chandeshwar <cschsinghaniya@gmail.com>
2022-04-08 14:18:28 +0530

    added responsive support

---
+++ public/js/script.js
@@ -0,0 +1,24 @@
+console.log('connected');
+const menuBtn = document.querySelector('i');
+const container = document.getElementsByClassName('chat__main')
+const sidebar = document.getElementsByClassName('chat__sidebar')
+
+var sideBarOpen = false
+
+menuBtn.addEventListener('click', (e) => {
+    console.log('clicked')
+    container[0].style.setProperty("--selection-background", "block");
+    e.stopPropagation()
+    sidebar[0].style.left = '0'
+    sidebar[0].style.boxShadow = 'rgb(0 0 0 / 50%) 0px 7px 29px 0px'
+    sideBarOpen = true
+})
+container[0].addEventListener('click', () => {
+    console.log('clicked container')
+    if (sideBarOpen) {
+        sidebar[0].style.left = '-225px'
+        sidebar[0].style.boxShadow = 'none'
+        container[0].style.setProperty("--selection-background", "none");
+        sideBarOpen = false
+    }
+})
